d7e6356111e7a3d2dd48d2eb5300c06d6c9f1b5f
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-02-05 23:26:30 +0800

    creating elbow method

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3, 4]].values
 
-print(X)
+from sklearn.cluster import KMeans
+
+WCSS = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters= i, init= 'k-means++', random_state= 42)
+    kmeans.fit(X=X)
+    WCSS.append(kmeans.inertia_)
+plt.plot(range(1, 11), WCSS )
+plt.title("ELBOW METHOD")
+plt.xlabel("Numbers of cluster")
+plt.ylabel("WCSS")
+plt.show()
