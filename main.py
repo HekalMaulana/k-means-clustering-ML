06d721062166604f702bc09efe5e1dbceb016dd3
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2024-02-08 14:22:49 +0800

    Visualing k-means cluster and create random color for each cluster

--- main.py
+++ main.py
@@ -32,9 +32,26 @@ for i in range(1, 11):
 # print(f"Ideal Number Cluster : {ideal_silhouette_score}")
 
 # Melatih k-means dengan dataset
-kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+ideal_num_cluster = 5
+kmeans = KMeans(n_clusters=ideal_num_cluster, init='k-means++', random_state=42)
 y_kmeans = kmeans.fit_predict(X)
 print(y_kmeans)
 
+# random color
+colors = np.random.rand(ideal_num_cluster, 3)
+
 # Visualisasi k-means
+for cluster_num in range(ideal_num_cluster):
+    plt.scatter(X[y_kmeans == cluster_num, 0], X[y_kmeans == cluster_num, 1], s=100, c=[colors[cluster_num]], label=f"Cluster {cluster_num + 1}")
+# plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+# plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+# plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+# plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+# plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 200, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
     
